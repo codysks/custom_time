48d0456f473c1348a01e784889cb4e58d2433a7c
Cody Shum <cody.shum@gmail.com>
2025-02-11 21:18:16 +0900

    Functional test file

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ CXXFLAGS = -Wall -Werror -Wextra -O3
 
 custom_time.cpp.o: custom_time.cpp
 	$(CXX) $(CXXSTDLANG) $(CXXFLAGS) -c -o $@ $<
+test: test.cpp custom_time.cpp.o
+	 $(CXX) $(CXXSTDLANG) $(CXXFLAGS) -o $@ $^
--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
 #include "custom_time.hpp"
 
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <unistd.h>
+#include <vector>
+
 #include <cassert>
 #ifdef NDEBUG
 # error "NDEBUG must not be set"
@@ -8,133 +14,142 @@
 #define assertnot(EXPR) assert(!(EXPR));
 
 int main(void) {
+	size_t section = 0;
+	size_t test_number = 0;
+	auto pt = [&](Time const& t, std::ostream& o = std::cout) {
+		o << section << ":" << test_number << ":";
+		o << t.second() << "." 
+			<< std::setfill('0') << std::setw(10) << t.nsecond() << std::endl;
+		++test_number;
+	};
+	{
+// Constructors
+//		Time(void);
+//		Time(decltype(TIME_UTC) time_utc);
+//		Time(time_t seconds, long nseconds);
+//		Time(struct timespec const& ts);
+//		Time(struct timespec&& ts);
+		Time tsnow{TIME_UTC};
+		auto pcont = std::vector<Time>{
+			Time{},
+			Time{TIME_UTC},
 
-    size_t section = 0;
-    size_t test_number = 0;
-    auto pt = [&](Time const& t, std::ostream& o = std::cout) {
-        o << section << ":" << test_number << ":";
-        o << t.second() << "." 
-            << std::setfill('0') << std::setw(10) << t.nsecond() << std::endl;
-        ++test_number;
-    };
-    {
-//        Time(void);
-//        Time(decltype(TIME_UTC) time_utc);
-//        Time(time_t seconds, long nseconds);
-//        Time(struct timespec const& ts);
-//        Time(struct timespec&& ts);
-        Time tsnow{TIME_UTC};
-        auto pcont = std::vector<Time>{
-            Time{},
-            Time{TIME_UTC},
+			Time{1739267836, 0},
+			Time{1739267836, 1},
+			Time{1739267836, 10},
+			Time{1739267836, 1000},
+			Time{1739267836, 999999998},
+			Time{1739267837, 999999999},
 
-            Time{1739267836, 0},
-            Time{1739267836, 1},
-            Time{1739267836, 10},
-            Time{1739267836, 1000},
-            Time{1739267836, 999999998},
-            Time{1739267837, 999999999},
+			Time{tsnow},
+			Time{Time{1739267838, 0}}
+		};
+		for (auto const& p : pcont) {
+			pt(p);
+		}
+	}
+// Comparison Operators
+//		bool operator>(Time const& rhs) const;
+//		bool operator<(Time const& rhs) const;
+//		bool operator==(Time const& rhs) const;
+//		bool operator!=(Time const& rhs) const;
+//		bool operator<=(Time const& rhs) const;
+//		bool operator>=(Time const& rhs) const;
+	{
+		Time t_hi  {1739267836, 750000001};
+		Time t_low {1000000000, 500000000};
+		assert(t_hi > t_low);
+		assert(t_hi >= t_low);
+		assert(!(t_hi == t_low));
+		assert(t_hi != t_low);
+		assert(!(t_hi <= t_low));
+		assert(!(t_hi < t_low));
+	}
+	{
+		Time t_hi  {1739267836, 499999999};
+		Time t_low {1000000000, 500000000};
+		assert(t_hi > t_low);
+		assert(t_hi >= t_low);
+		assert(!(t_hi == t_low));
+		assert(t_hi != t_low);
+		assert(!(t_hi <= t_low));
+		assert(!(t_hi < t_low));
+	}
+	{
+		Time t_hi  {1000000000, 000000000};
+		Time t_low { 999999999, 999999999};
+		assert(t_hi > t_low);
+		assert(t_hi >= t_low);
+		assert(!(t_hi == t_low));
+		assert(t_hi != t_low);
+		assert(!(t_hi <= t_low));
+		assert(!(t_hi < t_low));
+	}
+	{
+		Time t_hi  {1000000000, 500000001};
+		Time t_low {1000000000, 500000000};
+		assert(t_hi > t_low);
+		assert(t_hi >= t_low);
+		assert(!(t_hi == t_low));
+		assert(t_hi != t_low);
+		assert(!(t_hi <= t_low));
+		assert(!(t_hi < t_low));
+	}
+	{
+		Time t_mid  {1000000000, 500000000};
+		Time t_mid2 {1000000000, 500000000};
 
-            Time{tsnow},
-            Time{Time{1739267838, 0}}
-        };
-        for (auto const& p : pcont) {
-            pt(p);
-        }
-    }
-    // Comparison Operators
-//        bool operator>(Time const& rhs) const;
-//        bool operator<(Time const& rhs) const;
-//        bool operator==(Time const& rhs) const;
-//        bool operator!=(Time const& rhs) const;
-//        bool operator<=(Time const& rhs) const;
-//        bool operator>=(Time const& rhs) const;
-    {
-        Time t_hi  {1739267836, 750000001};
-        Time t_low {1000000000, 500000000};
-        assert(t_hi > t_low);
-        assert(t_hi >= t_low);
-        assert(!(t_hi == t_low));
-        assert(t_hi != t_low);
-        assert(!(t_hi <= t_low));
-        assert(!(t_hi < t_low));
-    }
-    {
-        Time t_hi  {1739267836, 499999999};
-        Time t_low {1000000000, 500000000};
-        assert(t_hi > t_low);
-        assert(t_hi >= t_low);
-        assert(!(t_hi == t_low));
-        assert(t_hi != t_low);
-        assert(!(t_hi <= t_low));
-        assert(!(t_hi < t_low));
-    }
-    {
-        Time t_hi  {1000000000, 000000000};
-        Time t_low { 999999999, 999999999};
-        assert(t_hi > t_low);
-        assert(t_hi >= t_low);
-        assert(!(t_hi == t_low));
-        assert(t_hi != t_low);
-        assert(!(t_hi <= t_low));
-        assert(!(t_hi < t_low));
-    }
-    {
-        Time t_hi  {1000000000, 500000001};
-        Time t_low {1000000000, 500000000};
-        assert(t_hi > t_low);
-        assert(t_hi >= t_low);
-        assert(!(t_hi == t_low));
-        assert(t_hi != t_low);
-        assert(!(t_hi <= t_low));
-        assert(!(t_hi < t_low));
-    }
-    {
-        Time t_mid  {1000000000, 500000000};
-        Time t_mid2 {1000000000, 500000000};
+		assertnot(t_mid > t_mid2);
+		assert(t_mid >= t_mid2);
 
-        assertnot(t_mid > t_mid2);
-        assert(t_mid >= t_mid2);
+		assert(t_mid == t_mid2);
+		assertnot(t_mid != t_mid2);
 
-        assert(t_mid == t_mid2);
-        assertnot(t_mid != t_mid2);
+		assert(t_mid <= t_mid2);
+		assertnot(t_mid < t_mid2);
+	}
 
-        assert(t_mid <= t_mid2);
-        assertnot(t_mid < t_mid2);
-    }
+	{
+		Time t_mid  {1000000000, 500000000};
+		Time const& t_mid2 = t_mid;
 
-    {
-        Time t_mid  {1000000000, 500000000};
-        Time const& t_mid2 = t_mid;
+		assertnot(t_mid > t_mid2);
+		assert(t_mid >= t_mid2);
 
-        assertnot(t_mid > t_mid2);
-        assert(t_mid >= t_mid2);
+		assert(t_mid == t_mid2);
+		assertnot(t_mid != t_mid2);
 
-        assert(t_mid == t_mid2);
-        assertnot(t_mid != t_mid2);
-
-        assert(t_mid <= t_mid2);
-        assertnot(t_mid < t_mid2);
-    }
-    // Rewind and forward functions
-    {
-        ++section; test_number = 0;
-        Time t1(10, 999999999);
-        pt(t1.get_rwd(1, 999999999));
-        pt(t1.get_rwd(1, 999999998));
-    }
-
-    {
-        ++section; test_number = 0;
-        Time p1{TIME_UTC};
-        pt(p1);
-        pt(p1.set());
-        pt(p1.set());
-        pt(p1.set());
-        sleep(1);
-        pt(p1.set());
-        pt(p1.set());
-        pt(p1.set());
-    }
-    return 0;
+		assert(t_mid <= t_mid2);
+		assertnot(t_mid < t_mid2);
+	}
+// Rewind and forward functions
+	{
+		++section; test_number = 0;
+		Time t1(10, 999999999);
+		pt(t1.pastby(1, 999999999));
+		pt(t1.pastby(1, 999999998));
+	}
+// Set functions
+	{
+		++section; test_number = 0;
+		Time p1{TIME_UTC};
+		pt(p1);
+		pt(p1.set());
+		pt(p1.set());
+		pt(p1.set());
+		sleep(1);
+		pt(p1.set());
+		pt(p1.set());
+		pt(p1.set());
+	}
+	{
+		++section; test_number = 0;
+		Time p1{TIME_UTC};
+		pt(p1);
+		while (p1.ffwdcmpnow(5, 0)) {
+			pt(Time{TIME_UTC});
+			sleep(1);
+		}
+	}
+	return EXIT_SUCCESS;
 }
